7ae40795b303e8d2bafea093d1d7687d54f39a83
fernandobonilla <fbr009@alumnos.ucn.cl>
2022-11-12 20:22:26 -0300

    Version 0.3
    URP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,36 +13,38 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float speed = 0.1f;
     
-    //Variable que almacena las monedas obtenidas por el jugar en numeros enteros.
-    public int coins = 0;
-
-    //Variable nombre del jugador
-    [SerializeField]
-    private string name = "Player";
     #endregion
     
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Juego Iniciado, el protagonista está listo para sacarse un 70 en esta prueba");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        restarMoneda();
-        izquierda();
+    
+        movimiento();
 
     }
 
-    void restarMoneda(){
-        //Restar moneda
-        if (Input.GetKeyDown(KeyCode.Space)){
-            coins--;
+    void movimiento(){
+
+        //Se mueve arriba
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0f, 0f, speed);
         }
-    }
-    void izquierda(){
+        //Se mueve abajo
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0f, 0f, -speed);
+        }
+        //Se mueve a la derecha.
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0f, 0f);
+        }
         //Se mueve a la izquierda.
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -50,16 +52,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    //Detecta cuando se realiza una interseccion
-    public void OnTriggerEnter(Collider collider){
-        Debug.Log("Se realizo una interseccion");
-        //Si colisiono con un gameobject con el tag "Moneda" pasa esto
-        if (collider.gameObject.CompareTag("Moneda"))
-        {
-            coins++;
-            Debug.Log("Usted intercepto con una moneda, así que le daré una moneda");
-        }
-    }
+
 
 
 
